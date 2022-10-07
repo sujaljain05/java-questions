de5f386fbd2e13881ee08ad68c862a0b777d1f82
SUJAL JAIN <106304459+sujaljain05@users.noreply.github.com>
2022-10-07 12:02:07 +0530

    Update first.java

--- first.java
+++ first.java
@@ -2,32 +2,31 @@ import java.util.Scanner;
 
 // 1. Write a program to print the area of a rectangle by creating a class named 'Area' having two methods. First method named as 'setDim' takes length and breadth of rectangle as parameters and the second method named as 'getArea' returns the area of the rectangle. Length and breadth of rectangle are entered through keyboard.
 
-class Area
-{
-    void setDim(int a, int b)
-    {
-        
-    }
-    int getArea(int a,int b) 
-    {
-        // setDim(a,b);
-        return a*b;
-    }
-} 
+class Area {
+    int l, w;
 
-public class first
-{
-    public static void main(String args[]) 
-    {
-        Scanner sc=new Scanner(System.in);
+    void setDim(int a, int b) {
+        l = a;
+        w = b;
+    }
+
+    int getArea() {
+        return l * w;
+    }
+}
+
+public class first {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the length of rectangle:");
-        int a=sc.nextInt();
+        int a = sc.nextInt();
         System.out.println("Enter the breadth of rectangle:");
-        int b=sc.nextInt();
-        
-        Area rec=new Area();
-        int area=rec.getArea(a,b);
-        System.out.println("The area of rectangle is:"+area);
+        int b = sc.nextInt();
+
+        Area rec = new Area();
+        rec.setDim(a, b);
+        int area = rec.getArea();
+        System.out.println("The area of rectangle is:" + area);
         sc.close();
     }
 }
